4a4d0651d80a85d1a55a5c12a719d7ff2a9eab02
Alex <m9939418@gmail.com>
2026-01-26 12:22:57 +0800

    Initial commit

---
+++ app/src/main/java/com/alex/yang/youtubecompose/App.kt
@@ -0,0 +1,12 @@
+package com.alex.yang.youtubecompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AlexYang on 2026/1/26.
+ *
+ *
+ */
+@HiltAndroidApp
+class App: Application()
